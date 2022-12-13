101dc74edd9cd226b3a9441f7ab9495647678d8f
Samuel de Carvalho <samuel.xw12@gmail.com>
2022-12-13 17:02:59 -0300

    Correção no length da descriçaõ de tarefas para ativação do btn. ( validations.js linha 7 )

--- src/tasks/validation.js
+++ src/tasks/validation.js
@@ -4,7 +4,7 @@ let inputNewTask = document.getElementById('newTaskInput');
 inputNewTask.addEventListener("keyup", () => {
   let btnSubmit = document.getElementById('submitTask');
 
-  if (inputNewTask.value){
+  if (inputNewTask.value.length > 5){
     btnSubmit.removeAttribute('hidden');
   } else {
     btnSubmit.setAttribute('hidden', true);
