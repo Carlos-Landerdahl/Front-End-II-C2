92195be353ada4efe4a80f102320bdc088b32ff8
Igor de Freitas Cruz <igor.freitas.cruz@icloud.com>
2022-12-14 20:40:21 -0300

    Animação no encerramento da sessão

--- src/utils.js
+++ src/utils.js
@@ -8,10 +8,25 @@ function baseUrl() {
 function closeApp() {
     // Encerra a aplicação no browser e direciona o usuário
     // para a tela de login
-    const encerrarSessao = confirm("Você deseja finalizar a sessão?")
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: true
+    })
 
-    if (encerrarSessao){
-        sessionStorage.removeItem('jwt')
-        location.href = "index.html"
-    }
+    swalWithBootstrapButtons.fire({
+        title: 'Deseja sair?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Sim',
+        cancelButtonText: 'Não',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            sessionStorage.removeItem("jwt")
+            location.href = "index.html"
+        }
+    })
 }
